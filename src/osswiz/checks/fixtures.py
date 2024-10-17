63628a95558706516ded53078fee8f6069e6832e
Adrian Rumpold <a.rumpold@gmail.com>
2024-10-17 08:54:27 +0200

    fix: Change fixture to use Traversable interface instead of Path

--- src/osswiz/checks/fixtures.py
+++ src/osswiz/checks/fixtures.py
@@ -1,8 +1,8 @@
-from os import PathLike
+from importlib.abc import Traversable
 from pathlib import Path
 
 
-def license_files(root: str | PathLike[str]) -> list[Path]:
+def license_files(root: Traversable) -> list[Path]:
     """Identify common license files in the repository"""
-
-    return list(Path(root).glob("LICENSE*")) + list(Path(root).glob("COPYING*"))
+    prefixes = ("LICENSE", "COPYING")
+    return [f for f in root.iterdir() if f.name.startswith(prefixes)]
